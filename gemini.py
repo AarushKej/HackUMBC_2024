aefac8f36369eb77376c2f395c5c9c59930dacc3
AarushKej <114349791+AarushKej@users.noreply.github.com>
2024-09-28 13:48:44 -0400

    Update gemini.py

--- gemini.py
+++ gemini.py
@@ -5,7 +5,6 @@ API_KEY= "AIzaSyDj9X15nYR83615JzaRTswdG2LrHZTcfIE"
 
 genai.configure(api_key=API_KEY)
 
-
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("Write a story about a magic backpack.")
 print(response.text)
