9336041d70722fb65a5056277cc21a74f89ea117
chubbybaby31 <49795066+chubbybaby31@users.noreply.github.com>
2024-09-28 12:59:55 -0400

    test

--- voice_listener.py
+++ voice_listener.py
@@ -1,15 +1,32 @@
 import speech_recognition as sr
+import pygame
+
+pygame.init()
 
 # Initialize recognizer class (for recognizing the speech)
 r = sr.Recognizer()
 
 # Reading Microphone as source
 # listening the speech and store in audio_text variable
-with sr.Microphone() as source:
-    audio_text = r.listen(source)
-    
-    try:
-        # using google speech recognition
-        print("Text: "+r.recognize_google(audio_text))
-    except:
-         print("Sorry, I did not get that")
+running = True
+activated = False
+command = ""
+while running: 
+    with sr.Microphone() as source:
+        audio_text = r.listen(source)
+        try:
+            query = r.recognize_google(audio_text)
+            if (activated):
+                command = query
+                activated = False
+            else: command = ""
+            if ('Jarvis' in query or 'jarvis' in query):
+                activated = True
+                pygame.mixer.music.load("JARVIS_Awake.wav")
+                pygame.mixer.music.play()
+                pygame.event.wait()
+                print("TALK")
+        except:
+            print("Sorry, I did not get that")
+
+        if command != "": print(command)
