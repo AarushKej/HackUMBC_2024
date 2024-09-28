e16fe7ab6f44d956e30cdc8b75005a28157ecdff
AarushKej <114349791+AarushKej@users.noreply.github.com>
2024-09-28 12:39:56 -0400

    Update screen_recorder.py

--- screen_recorder.py
+++ screen_recorder.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cv2
 from PIL import ImageGrab
+x=1
+while(x==1):
+    x +=1
+    screen = np.array(ImageGrab.grab(bbox=(0,40,2560,1664)))
 
-while(True):
-    screen = np.array(ImageGrab.grab(bbox=(0,10,2650,1080)))
-
+while(True):  
     cv2.imshow('window', cv2.cvtColor(screen, cv2.COLOR_BGR2RGB))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
