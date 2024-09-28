d9fc4b22da6288ecb9857594829ff3805023b7a8
chubbybaby31 <49795066+chubbybaby31@users.noreply.github.com>
2024-09-28 13:00:19 -0400

    key

--- gemini.py
+++ gemini.py
@@ -2,6 +2,8 @@ from gemini import Gemini
 
 client = Gemini(auto_cookies=True)
 
+api_key = "AIzaSyDj9X15nYR83615JzaRTswdG2LrHZTcfIE"
+
 
 response = client.generate_content("Hello, Gemini. What's the weather like in Seoul today?")
 print(response.payload)
